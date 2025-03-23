a0cf5e04947aaf5dfe6f9d485cea3d5326c31595
geraldlloyd02 <geraldlloyd112@gmail.com>
2025-03-23 16:33:52 +0800

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+import { getAuth ,signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAuxCGPK4u_ZlSGOG5Q7HUJS7ZoA_vGy-U",
+    authDomain: "login-and-register-2046c.firebaseapp.com",
+    projectId: "login-and-register-2046c",
+    storageBucket: "login-and-register-2046c.firebasestorage.app",
+    messagingSenderId: "701658265318",
+    appId: "1:701658265318:web:f492baefc8a9276b54ed27"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    //input fields
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        alert("Logged in")
+        window.location.href="profile.html";
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+        // ..
+    });
+    
+
+})
